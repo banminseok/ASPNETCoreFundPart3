97c07fca59a295c38182209b77161c6fdcba1158
ban <halfmin@gmail.com>
2021-04-02 18:02:10 +0900

    mdl add

---
+++ DotNetNote.Models/Companies/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetNote.Models.Companies
+{
+    class Class1
+    {
+    }
+}
