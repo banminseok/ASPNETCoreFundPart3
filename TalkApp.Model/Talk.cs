6bd2399d4ac0e8df67ad15276eadf77112126003
ban <halfmin@gmail.com>
2021-04-02 15:01:48 +0900

    TalkApp 셋팅

---
+++ TalkApp.Model/Talk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TalkApp.Models
+{
+    public class Talk
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        //작성일자
+        public DateTime? Created { get; set; }
+
+        public List<TalkComment> TalkComments { get; set; } = new List<TalkComment>(); // 일대 다 관계 설정.
+        public string? UserId { get; set; }
+    }
+}
---
+++ TalkApp.Model/TalkComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TalkApp.Models
+{
+    public class TalkComment
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        //작성일자
+        public DateTime? Created { get; set; }
+
+        public Talk Talk { get; set; } //--부모참조
+        public long TalkId { get; set; } //-- 부모아이디
+
+        public string? UserId { get; set; }
+    }
+}
