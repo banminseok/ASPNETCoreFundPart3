8697c8f67e7733debd24c754c481e36a15b7364e
ban <halfmin@gmail.com>
2021-04-12 16:58:08 +0900

    Attendees Setting

---
+++ DotNetNote.SqlServer/dbo/Tables/Attendees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Attendees]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1000,1),		--일련번호
+	[UID]	Int Not Null ,				-- 사용자 UID(fn)
+	[UserID]	Nvarchar(25) Not Null ,				-- 사용자 ID(fn)
+	[Name]		Nvarchar(25) Not Null,				-- 참석자 이름
+	Mobile	Nvarchar(25) Null,						-- 휴대폰번호
+	[CreationDate]	DateTime Default(Getdate())		-- 등록일
+)
---
+++ DotNetNote.SqlServer/dbo/Tables/CommunityCampJoinMembers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CommunityCampJoinMembers]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),		--일련번호
+	ComunityName	Nvarchar(25) Not Null,
+	[Name]		Nvarchar(25) Not Null,				-- 참석자 이름
+	Mobile	Nvarchar(25) Not Null,						-- 휴대폰번호
+	Email	Nvarchar(25) Not Null,
+	Size	Nvarchar(25) Not Null Default('L'),				-- 티셔츠 기념품 사이즈
+	CreationDate	DateTime Dafault(Getdate())		-- 등록일
+)
